04bab1637742dc75203864de46325e4507045a68
Layan168 <Layan0143@gmail.com>
2025-02-27 12:02:37 +0300

    Add files via upload

---
+++ mainClass.java
@@ -0,0 +1,10 @@
+package projectphase1;
+
+public class mainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is main class");
+	}
+
+}
