e373694ecc730f654e9714fc1cc11677d82c621f
lama-alghamdi378 <lama13572020@gmail.com>
2025-03-02 19:38:40 +0300

    Update order.java

--- order.java
+++ order.java
@@ -5,6 +5,24 @@ public class order {
 	public int ID; //customer's id (will be used in search method in library class)
 	books orderItems[]; //array with the books the customer wants to purchase
 	public int numOfItems; //number of books the order
+	public double actualPrice; //the price of all the books
+	
+	public order (String n, int id, int noi) {
+		name = n;
+		ID = id;
+		numOfItems = noi;
+	}
+	
+	public order(order o) {
+		name = o.name;
+		ID = o.ID;
+		numOfItems = o.numOfItems;
+		
+	}
+	
+	//add item, remove item, search for an item, 
+	
+	
 	
 	
 
