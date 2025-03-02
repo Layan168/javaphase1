3e7404e18c4fe83e8b4f88946a67a86a33a85969
lama-alghamdi378 <lama13572020@gmail.com>
2025-03-02 23:13:37 +0300

    Update order.java

--- order.java
+++ order.java
@@ -7,21 +7,44 @@ public class order {
 	public int numOfItems; //number of books the order
 	public double actualPrice; //the price of all the books
 	
-	public order (String n, int id, int noi) {
+	public order (String n, int id, int size) {
 		name = n;
 		ID = id;
-		numOfItems = noi;
+		orderItems = new books [size];
+		numOfItems = 0;
 	}
 	
-	public order(order o) {
-		name = o.name;
-		ID = o.ID;
-		numOfItems = o.numOfItems;
+	public boolean addItem (books a) { //check if this is the right way to add up the price
+		if (numOfItems < orderItems.length) {
+			orderItems[numOfItems] = new books(a); //composition but it doesn't work!
+			numOfItems++;
+			actualPrice += a.price;
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean removeItem(String title) {
+		for (int i= 0; i < numOfItems; i++) {//make sure equalsIgnoreCase is the right one
+			if (orderItems[i].title.equalsIgnoreCase(title)) {
+				actualPrice -= orderItems[i].price;
+				orderItems[i] = orderItems[numOfItems-1];
+				numOfItems --;
+				return true;
+			}
+		}
+			return false;
 		
 	}
-	
-	//add item, remove item, search for an item, 
-	
+		
+	public boolean searchItem(String title) {
+		for (int i = 0; i <numOfItems; i++) {
+			if (orderItems[i].title.equalsIgnoreCase(title))
+				return true;
+		}
+		return false;
+		
+	}
 	
 	
 	
