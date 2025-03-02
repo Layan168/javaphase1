c87dd335c22ce7d7b4f086a068eac6b06d46aa91
xxhajer <hajerthe911@gmail.com>
2025-03-02 05:11:16 +0300

    Update Fiction.java

--- Fiction.java
+++ Fiction.java
@@ -1,5 +1,12 @@
 package finction;
 
-public class Fiction {
-
+public class Fiction extends books {
+protected int numOfpages; //number of pages in book
+protected String bookClassification;  //suspense-drama-action
+protected String bookSeries; //part 2 from book gg    
+  public Fiction(String t, String a, String p, double pr, int y , int noc, int o, String fic, String Ser){
+    super(t,a,p,pr,y,noc);
+    numOfpages=o;
+    bookClassification = fic;
+    bookSeries = Ser;
 }
