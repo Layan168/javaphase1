7f82975e1ce0a62acd3595ff37624ac53e463286
lama-alghamdi378 <lama13572020@gmail.com>
2025-03-02 19:36:47 +0300

    Update library.java

--- library.java
+++ library.java
@@ -3,16 +3,48 @@ package javaphase1;
 public class library {
 	public String name; //name of the library
 	public String location; //the location of the library
-	public int numWorkers; //the number of workers (should we remove this attribute?)
+	public int numWorkers; //the number of workers 
 	public int nob; //number of books in the array
-	public int noo; //number of orders in the array (should we change the name?)
+	public int noo; //number of orders in the array 
 	books inStore []; //array containing all the books in the library
-	order ListOfOrders[]; //an array with all the orders that were ever made
+	order ListOfOrders[];
+	
+	public library(String n , String loc) {
+		name = n;
+		location = loc;
+		inStore = new books[500];
+		ListOfOrders = new order[999];
+		nob = 0 ;
+		noo = 0;
+	}
+	
+	public boolean addBook (books b) { //this is supposed to be able to take a book object(that we just created) and add it to the list of the books available in this specific library
+		if (nob < inStore.length) {
+			inStore[nob ++] = b;
+			return true;
+		}
+		return false;
+		
+	}
+	
+	public boolean addOrder(order o) {
+		if(noo < ListOfOrders.length) {
+			ListOfOrders[noo++] = o;
+			return true;
+		}
+		return false;
+	}
+	
+	
+	
+	//an array with all the orders that were ever made
 	/* methods that will be added later(not complete)
 	add book
 	add order
-	cancel order 
-	cancel book
+	remove order 
+	remove book
+	search
+	
 	*/
 
 }
