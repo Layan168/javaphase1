b13edef51fdd21ac144fdd102900823af79d9791
Layan168 <Layan0143@gmail.com>
2025-03-02 23:51:53 +0300

    Update Non_fiction.java

--- Non_fiction.java
+++ Non_fiction.java
@@ -1,5 +1,18 @@
 package project;
 
 public class Non_fiction {
-
+public NOn_fiction(String t,String a,string p,double pr,int y,int noc,String s,int e) {
+		super(t,a,p,pr,y,noc);
+		sbject=s;
+		edition=e;
+	}
+  
+  public double specialDiscount(){
+    if(getnoc()>10)
+      return getprice() * 0.80;
+  } else {
+    return getprice() * 0.90;
+    
+    public String tostring(){
+      return super.toString()+", Subject:"+subject+", Edition:"+edition;
 }
