ac91acfa59911e1fe5fb47a9f3780895eec432e3
lama-alghamdi378 <lama13572020@gmail.com>
2025-03-02 04:35:55 +0300

    Update books.java

--- books.java
+++ books.java
@@ -8,6 +8,15 @@ public abstract class books {
 	protected int year; //the year the book was published in
 	protected int noc; //number of copies available
 	
+	public books (String t, String a, String p, double pr, int y, int noc) {
+	title = t;
+	author = a;
+	publisher = p;
+	price = pr;
+	year = y;
+	this.noc = noc;
+	}
+	
 	public abstract double specialDiscount(); //gives a discount based on the type of the book and other factors
 	
 	public double  discount(int percent) { //gives a discount by percent
@@ -15,4 +24,59 @@ public abstract class books {
 	}
 	//i made both of the methods double because the price is double so we son't have to cast anything
 	//remove this comment later**
+	
+	public String toString() {
+		return "title: " + title + "  author: " + author + "  publisher: "+ publisher+ "  price: " + price + "  year: "+ year + "number of copies available: " + noc;
+	}
+	
+	//setters and getters
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public String getPublisher() {
+		return publisher;
+	}
+
+	public void setPublisher(String publisher) {
+		this.publisher = publisher;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	public int getNoc() {
+		return noc;
+	}
+
+	public void setNoc(int noc) {
+		this.noc = noc;
+	}
+	
 }
