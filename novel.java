0e56f1809fa7533f2739c77ddf25c1d94005e0d8
lama-alghamdi378 <lama13572020@gmail.com>
2025-03-04 00:16:59 +0300

    Update novel.java

--- novel.java
+++ novel.java
@@ -14,13 +14,14 @@ public void setRecommendedAge(int age){
   }
   public double specialDiscount(){
     if(numOfpages>800)
-      return 0.25;
+      return price - (price *0.25);
     else
       if(numOfpages>600)
-        return 0.15;
+        return price - (price *0.15);
     else
         if(numOfpages>400)
-          return 0.10;
+          return price - (price * 0.10);
+    return price;
   }
   public String toString(){
     return super.toString()+"   recommended Age for novel book"+recommendedAge;
