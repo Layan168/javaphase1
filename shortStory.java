157198ea74b72e0632d529095b65fc0e65f0842e
xxhajer <hajerthe911@gmail.com>
2025-03-03 22:57:21 +0300

    Update shortStory.java

--- shortStory.java
+++ shortStory.java
@@ -1,5 +1,22 @@
 package finction;
 
 public class shortStory extends fiction{
+private int countWord;
 
+  public shortStory(String t, String a, String p, double pr, int y, int noc, int o, String fic, String ser, int cou){
+    super(t,a,p,pr,y,noc,o,fic,ser);
+    countWord= cou;
+    
 }
+public double  Discount(){
+  if(countWord>3000)
+    return 0.25;
+  else
+    if(countWord > 2000)
+      return 0.15;
+  else
+      if(countWord > 1000)
+        return 0.05;
+}
+  public String toString(){
+    return super.toString()+"countWord of short Story"+countWord;
