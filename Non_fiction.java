a3a2e9bed5aa28d840df24b807527661c25d4c6c
Layan168 <Layan0143@gmail.com>
2025-03-04 00:35:41 +0300

    Update Non_fiction.java

--- Non_fiction.java
+++ Non_fiction.java
@@ -1,24 +1,46 @@
 package project;
-
+import java.util.Scanner;
 public class Non_fiction {
-	private String subject;
-	private int edition;
+	
+	private int yearOfedition;
 	private String language;
 	
 public NOn_fiction(String t,String a,string p,double pr,int y,int noc,String s,int e,String lg) {
 		super(t,a,p,pr,y,noc);
-		sbject=s;
+		subject=s;
 		edition=e;
 	        language=lg;
 	}
   
   public double specialDiscount(){
-	  double newPrice=0;
-    if(getnoc()>10)
-      return getprice() * 0.80;
-  } else {
-    return getprice() * 0.90;
-    
+	Scanner input=new Scanner.(system.in);
+	  System.out.println("Please enter the current year:");
+	  int currentYear=input.nextInt();
+	  
+	  int ageOfEdition=currentYear-yearOfedition;
+	  double discountPercentage=0;
+	  if(ageOfEdition<=1)
+		 discountPercentage=0.05;
+	  else 
+	  if(ageOfEdition<=3)
+		 discountPercentage=0.10;
+	  else
+	if(ageOfEdition<=5)
+		 discountPercentage=0.15;	
+	  else
+		 discountPercentage=0.20;
+	 
+	 double discountValue=getPrice()* discountPercentage;
+	  return getPrice()-discountValue;
+	  
+	  
+     public void isTranslated(){    // check if book has translation based on the language
+	     if(language.equalsIgnoreCase("English")||language.equalsIgnoreCase("Arabic")||language.equalsIgnoreCase("French"))
+		     System.out.println("Yes,it has translation.");
+	     else
+		    System.out.println("Unfortunately,it doesn’t have translation.");  
+
+	     
     public String tostring(){
       return super.toString()+", Subject:"+subject+", Edition:"+edition;
 }
