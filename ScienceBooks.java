e0fa547199156c12eaf4a27ceb985f39e511d39e
Layan168 <Layan0143@gmail.com>
2025-03-04 00:49:11 +0300

    Update ScienceBooks.java

--- ScienceBooks.java
+++ ScienceBooks.java
@@ -1,5 +1,13 @@
 package finction;
 
-public class ScienceBooks {
+public class ScienceBooks extends Non_fiction {
 
+  public void checkForRecentUpdates(int currentYear){
+    int yearsSincePublication=currentYear-getYearOfEdition();
+    if(yearsSincePublication<=2)
+      System.out.println("This book has the lastest scientific updates.")
+        else
+     System.out.println("This book may not have the lastest scientific updates.") 
+
+      
 }
