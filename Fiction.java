9b7a7544874b33b6f3a004e24a27143a8f288b70
xxhajer <hajerthe911@gmail.com>
2025-03-04 00:10:10 +0300

    Update Fiction.java

--- Fiction.java
+++ Fiction.java
@@ -7,17 +7,17 @@ protected String Series; //part 2 from book gg
   public Fiction(String t, String a, String p, double pr, int y , int noc, int o, String fic, String Ser){
     super(t,a,p,pr,y,noc);
     numOfpages=o;
-    bookClassification = fic;
-    bookSeries = Ser;
+    genre = fic;
+   Series = Ser;
 }
-public double Discount(){
-  return 0.0
+public double specialDiscount(){
+  return  0.0 ;
     }
   public String toString(){
-    return super.toString()+"Number Of pages:"+numOfpages+"genre of finction:"+genre+"Book Series:"+Series;
+    return super.toString()+"     Number Of pages:"+numOfpages+"    genre of finction:"+genre+"    Book Series:"+Series;
   }
   public void updateGenre(String Ngenre){
-    this.genre = Ngenre;
+   genre = Ngenre;
   }
   public boolean isPartOfSeries(){
-    return (this.Series !=null&& !this.Series.isEmpty());
+    return (!Series.equlas(null));
