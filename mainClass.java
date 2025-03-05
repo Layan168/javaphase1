567fcc3b2d4bce20322ca6fc19fe04c4db2515ff
lama-alghamdi378 <lama13572020@gmail.com>
2025-03-05 09:49:55 +0300

    Update mainClass.java

--- mainClass.java
+++ mainClass.java
@@ -5,6 +5,10 @@ public class mainClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("this is main class");
+		//create objects first (normal ones, no polymorphism)
+		//put the objects/books in the library array
+		//create the loop for menu (what are the options that should be included in the menu?)
+		//we can either directly write the code for each case or use a static method (only benifit is minimizing errors
 	}
 
 }
