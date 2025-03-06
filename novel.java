9896748d317da22984f044b4cac2338e8072dcf7
lama-alghamdi378 <lama13572020@gmail.com>
2025-03-06 13:41:10 +0300

    Update novel.java

--- novel.java
+++ novel.java
@@ -1,28 +1,34 @@
 package javaphase1;
 
-public class novel extends fiction {
-protected int recommendedAge;// less than 18 or for 5 to 9 
-  public novel (String t, String a, String p, double pr, int y, int noc, int o , String fic, String ser, int rec){
-    super(t,a,p,pr,y,noc,o,fic,ser,rec);
-    recommendedAge = rec;
-}
-public void setRecommendedAge(int age){
-  recommendedAge = age;
-}
-  public int getRecommendedAge(){
-    return recommendedAge;
-  }
-  public double specialDiscount(){
-    if(numOfpages>800)
-      return price - (price *0.25);
-    else
-      if(numOfpages>600)
-        return price - (price *0.15);
-    else
-        if(numOfpages>400)
-          return price - (price * 0.10);
-    return price;
-  }
-  public String toString(){
-    return super.toString()+"   recommended Age for novel book"+recommendedAge;
+public class Novel extends Fiction {
+	protected int recommendedAge;// less than 18 or for 5 to 9 
+	  public Novel (String t, String a, String p, double pr, int y, int noc, int o , String fic, String ser, int rec){
+	    super(t,a,p,pr,y,noc,o,fic,ser);
+	    recommendedAge = rec;
+	}
+	
+	  public double specialDiscount(){
+	    if(numOfpages>800)
+	      return price = price - (price *0.25);
+	    else
+	      if(numOfpages>600)
+	        return price = price - (price *0.15);
+	    else
+	        if(numOfpages>400)
+	          return price = price - (price * 0.10);
+	        else
+	        	return price;
+	  }
+	  public String toString(){
+	    return super.toString()+"  recommended Age for novel book: "+recommendedAge;
+	}
+	  
+	 public void setRecommendedAge(int age){
+		  recommendedAge = age;
+		}
+	 public int getRecommendedAge(){
+		    return recommendedAge;
+		  }
+	  
+
 }
