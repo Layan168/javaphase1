9e5d84f4fb9886cef77be52feaa103ae67e6977c
lama-alghamdi378 <lama13572020@gmail.com>
2025-03-06 13:40:53 +0300

    Update mainClass.java

--- mainClass.java
+++ mainClass.java
@@ -1,14 +1,36 @@
-package projectphase1;
+package javaphase1;
 
-public class mainClass {
+public class MainClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("this is main class");
-		//create objects first (normal ones, no polymorphism)
-		//put the objects/books in the library array
-		//create the loop for menu (what are the options that should be included in the menu?)
-		//we can either directly write the code for each case or use a static method (only benifit is minimizing errors
+		 Novel novel1 = ("The One and Only Ivan", ); //12 books, non fiction, fiction, short stpry, novel, science (4 books)
+		 
+		 
+		 
+				 
+				 //insert library name
+				 
+				 //menu loop
+		 			//managing the library
+		 			//* add book --> what type --> insert information
+		 			//* search book --> title
+					//* delete book --> title
+		 			//* show book list --> list of books 
+		 
+		 			
+		 			//managing the orders
+				 	//$ buy book(add order) --> name and id --> show list of books --> insert title --> order info
+		 			//$ add discount --> order id --> discount percent --> show price after discount
+		 			//$ add special discount --> order id --> apply discount for all items based on type --> show price after discont
+		 			//__ search inside order --> book title --> is it there or not
+		 			//__ search for an order ---> id 
+		 			//__ delete order item --> name and id (check if there) --> show items in order --> insert book title --> show if book was actually deleted
+		 			//__ delete order --> id --> deleted
+		 			//$ show order list --> list of orders
+		 
+		 			//exit system.exit(0);
+		 			
+
 	}
 
 }
