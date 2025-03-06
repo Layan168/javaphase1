1bafccf3e32961dd81934310f760fbc56189d384
lama-alghamdi378 <lama13572020@gmail.com>
2025-03-06 13:41:24 +0300

    Update order.java

--- order.java
+++ order.java
@@ -1,26 +1,42 @@
 package javaphase1;
 
-public class order {
+public class Order {
 	public String name; //customer's name
-	public int ID; //customer's id (will be used in search method in library class)
-	books orderItems[]; //array with the books the customer wants to purchase
+	public int ID; //customer's id 
+	Books orderItems[]; //array with the books the customer wants to purchase
 	public int numOfItems; //number of books the order
-	public double actualPrice; //the price of all the books
 	
-	public order (String n, int id, int size) {
+	
+	public Order (String n, int id, int size) {
 		name = n;
 		ID = id;
-		orderItems = new books [size];
+		orderItems = new Books [size];
 		numOfItems = 0;
 	}
 	
-	public boolean addItem (books a) { //check if this is the right way to add up the price
+	public boolean addItem (Books a) { 
 		if (numOfItems < orderItems.length) {
 			if (a.getNoc() >0) {
-			orderItems[numOfItems] = new books(a); //composition but it doesn't work!
-			numOfItems++;
-			actualPrice += a.price;
-			a.setNoc() = a.getNoc() - 1;
+				if (a instanceof Novel)
+					orderItems[numOfItems] = new Novel(a.getTitle(), a.getAuthor(), a.getPublisher(), a.getPrice(), a.getYear(), a.getNoc(), ((Fiction)a).getNumOfpages(), ((Fiction)a).getGenre(), ((Fiction)a).getSeries(), ((Novel)a).getRecommendedAge());  
+				    numOfItems++;
+				    a.setNoc(a.getNoc() - 1);
+				 if (a instanceof ShortStory)
+					 orderItems[numOfItems] = new ShortStory(a.getTitle(), a.getAuthor(), a.getPublisher(), a.getPrice(), a.getYear(), a.getNoc(), ((Fiction)a).getNumOfpages(), ((Fiction)a).getGenre(), ((Fiction)a).getSeries(), ((ShortStory)a).getCountWord()); 
+					 numOfItems++;
+					 a.setNoc(a.getNoc() - 1);
+				if (a instanceof Science)
+					orderItems[numOfItems] = new Science(a.getTitle(), a.getAuthor(), a.getPublisher(), a.getPrice(), a.getYear(), a.getNoc(), ((NonFiction)a).getEdition(),((NonFiction)a).getLanguage(), ((Science)a).getScientificField());  
+					numOfItems++;
+					a.setNoc(a.getNoc() - 1);
+				if (a instanceof NonFiction)
+					orderItems[numOfItems] = new NonFiction(a.getTitle(), a.getAuthor(), a.getPublisher(), a.getPrice(), a.getYear(), a.getNoc(), ((NonFiction)a).getEdition(),((NonFiction)a).getLanguage()); 
+					numOfItems++;
+					a.setNoc(a.getNoc() - 1);
+				if (a instanceof Fiction)
+					orderItems[numOfItems] = new Fiction(a.getTitle(), a.getAuthor(), a.getPublisher(), a.getPrice(), a.getYear(), a.getNoc(), ((Fiction)a).getNumOfpages(), ((Fiction)a).getGenre(), ((Fiction)a).getSeries()); 
+					numOfItems++;
+					a.setNoc(a.getNoc() - 1);
 			return true;
 			}
 			return false;
@@ -31,8 +47,7 @@ public class order {
 	public boolean removeItem(String title) {
 		for (int i= 0; i < numOfItems; i++) {//make sure equalsIgnoreCase is the right one
 			if (orderItems[i].title.equalsIgnoreCase(title)) {
-				actualPrice -= orderItems[i].price;
-				orderItems[i].setNoc() = a.getNoc() +1;
+				orderItems[i].setNoc(orderItems[i].getNoc() +1);
 				orderItems[i] = orderItems[numOfItems-1];
 				numOfItems --;
 				
@@ -49,6 +64,25 @@ public class order {
 				return true;
 		}
 		return false;
+		
+	}
+	
+	public String toString() {
+		String str =" order name: " + name +"  customer's ID: " + ID + "  number of books: " + numOfItems ;
+		str = str + "/n items in the order: /n";
+		for (int i = 0; i< numOfItems; i++) {
+			str = str + orderItems[i].toString();
+			System.out.println();
+			
+		}
+		double totalPrice = 0.0;
+		str = str + "/n total price: ";
+		for (int i = 0; i< numOfItems; i++) {
+			totalPrice = totalPrice + orderItems[i].getPrice();
+			
+		}
+		
+		return str + totalPrice; 
 		
 	}
 	
