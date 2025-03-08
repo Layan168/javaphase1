af0d9048f0fade4d05c9f83d52a5a9416affc0cf
lama-alghamdi378 <lama13572020@gmail.com>
2025-03-08 08:14:32 +0300

    Update order.java

--- order.java
+++ order.java
@@ -2,12 +2,12 @@ package javaphase1;
 
 public class Order {
 	public String name; //customer's name
-	public int ID; //customer's id 
+	public String ID; //customer's id 
 	Books orderItems[]; //array with the books the customer wants to purchase
 	public int numOfItems; //number of books the order
 	
 	
-	public Order (String n, int id, int size) {
+	public Order (String n, String id, int size) {
 		name = n;
 		ID = id;
 		orderItems = new Books [size];
@@ -16,38 +16,37 @@ public class Order {
 	
 	public boolean addItem (Books a) { 
 		if (numOfItems < orderItems.length) {
-			if (a.getNoc() >0) {
-				if (a instanceof Novel)
-					orderItems[numOfItems] = new Novel(a.getTitle(), a.getAuthor(), a.getPublisher(), a.getPrice(), a.getYear(), a.getNoc(), ((Fiction)a).getNumOfpages(), ((Fiction)a).getGenre(), ((Fiction)a).getSeries(), ((Novel)a).getRecommendedAge());  
-				    numOfItems++;
-				    a.setNoc(a.getNoc() - 1);
-				 if (a instanceof ShortStory)
-					 orderItems[numOfItems] = new ShortStory(a.getTitle(), a.getAuthor(), a.getPublisher(), a.getPrice(), a.getYear(), a.getNoc(), ((Fiction)a).getNumOfpages(), ((Fiction)a).getGenre(), ((Fiction)a).getSeries(), ((ShortStory)a).getCountWord()); 
-					 numOfItems++;
-					 a.setNoc(a.getNoc() - 1);
-				if (a instanceof Science)
-					orderItems[numOfItems] = new Science(a.getTitle(), a.getAuthor(), a.getPublisher(), a.getPrice(), a.getYear(), a.getNoc(), ((NonFiction)a).getEdition(),((NonFiction)a).getLanguage(), ((Science)a).getScientificField());  
-					numOfItems++;
-					a.setNoc(a.getNoc() - 1);
-				if (a instanceof NonFiction)
-					orderItems[numOfItems] = new NonFiction(a.getTitle(), a.getAuthor(), a.getPublisher(), a.getPrice(), a.getYear(), a.getNoc(), ((NonFiction)a).getEdition(),((NonFiction)a).getLanguage()); 
-					numOfItems++;
-					a.setNoc(a.getNoc() - 1);
-				if (a instanceof Fiction)
-					orderItems[numOfItems] = new Fiction(a.getTitle(), a.getAuthor(), a.getPublisher(), a.getPrice(), a.getYear(), a.getNoc(), ((Fiction)a).getNumOfpages(), ((Fiction)a).getGenre(), ((Fiction)a).getSeries()); 
-					numOfItems++;
-					a.setNoc(a.getNoc() - 1);
+			
+				if (a instanceof Novel) {
+					orderItems[numOfItems] = new Novel(a.getTitle(), a.getAuthor(), a.getPublisher(), a.getPrice(), a.getYear(), ((Fiction)a).getNumOfpages(), ((Fiction)a).getGenre(), ((Fiction)a).getSeries(), ((Novel)a).getRecommendedAge());  
+				    numOfItems++;}
+				    
+				else if (a instanceof ShortStory) {
+					 orderItems[numOfItems] = new ShortStory(a.getTitle(), a.getAuthor(), a.getPublisher(), a.getPrice(), a.getYear(), ((Fiction)a).getNumOfpages(), ((Fiction)a).getGenre(), ((Fiction)a).getSeries(), ((ShortStory)a).getCountWord()); 
+					 numOfItems++;}
+					 
+				else if (a instanceof Science) {
+					orderItems[numOfItems] = new Science(a.getTitle(), a.getAuthor(), a.getPublisher(), a.getPrice(), a.getYear(), ((NonFiction)a).getEdition(),((NonFiction)a).getLanguage(), ((Science)a).getScientificField());  
+					numOfItems++;}
+					
+				else if (a instanceof NonFiction) {
+					orderItems[numOfItems] = new NonFiction(a.getTitle(), a.getAuthor(), a.getPublisher(), a.getPrice(), a.getYear(), ((NonFiction)a).getEdition(),((NonFiction)a).getLanguage()); 
+					numOfItems++;}
+					
+				else if (a instanceof Fiction) {
+					    orderItems[numOfItems] = new Fiction(a.getTitle(), a.getAuthor(), a.getPublisher(), a.getPrice(), a.getYear(), ((Fiction)a).getNumOfpages(), ((Fiction)a).getGenre(), ((Fiction)a).getSeries()); 
+					    numOfItems++;}
+					
 			return true;
 			}
-			return false;
-		}
+			
+		
 		return false;
 	}
 	
 	public boolean removeItem(String title) {
-		for (int i= 0; i < numOfItems; i++) {//make sure equalsIgnoreCase is the right one
+		for (int i= 0; i < numOfItems; i++) {
 			if (orderItems[i].title.equalsIgnoreCase(title)) {
-				orderItems[i].setNoc(orderItems[i].getNoc() +1);
 				orderItems[i] = orderItems[numOfItems-1];
 				numOfItems --;
 				
@@ -76,18 +75,71 @@ public class Order {
 		return tpr;
 	}
 	
-	public double addSpecialDiscount() {
+	public double addSpecialDiscountFiction() {
 		double tpr = 0;
-		for (int i = 0; i <numOfItems; i++) {
-			tpr = tpr + orderItems[i].specialDiscount();
-		}
+		for (int i = 0; i <numOfItems; i++) 
+			if(orderItems[i] instanceof Fiction && !(orderItems[i] instanceof Novel) && !(orderItems[i] instanceof ShortStory)) {
+				
+		for (int j = 0; j <numOfItems; j++) {
+			tpr = tpr + orderItems[j].price;
+		}}
 		
 		return tpr;
 	}
 	
+	public double addSpecialDiscountNonFiction() {
+		double tpr = 0;
+		for (int i = 0; i <numOfItems; i++) 
+			if(orderItems[i] instanceof NonFiction && !(orderItems[i] instanceof Science) ) {
+				
+		for (int j = 0; j <numOfItems; j++) {
+			tpr = tpr + orderItems[j].price;
+		}}
+		
+		return tpr;
+	}
+	
+	public double addSpecialDiscountScience() {
+		double tpr = 0;
+		for (int i = 0; i <numOfItems; i++) 
+			if(orderItems[i] instanceof Science) {
+				
+		for (int j = 0; j <numOfItems; j++) {
+			tpr = tpr + orderItems[j].price;
+		}}
+		
+		return tpr;
+	}
+	
+	public double addSpecialDiscountNovel() {
+		double tpr = 0;
+		for (int i = 0; i <numOfItems; i++) 
+			if(orderItems[i] instanceof Novel) {
+				
+		for (int j = 0; j <numOfItems; j++) {
+			tpr = tpr + orderItems[j].price;
+		}}
+		
+		return tpr;
+	}
+	
+	public double addSpecialDiscountShortStory() {
+		double tpr = 0;
+		for (int i = 0; i <numOfItems; i++) 
+			if(orderItems[i] instanceof ShortStory) {
+				
+		for (int j = 0; j <numOfItems; j++) {
+			tpr = tpr + orderItems[j].price;
+		}}
+		
+		return tpr;
+	}
+	
+	
+	
 	public String toString() {
-		String str =" order name: " + name +"  customer's ID: " + ID + "  number of books: " + numOfItems ;
-		str = str + "/n items in the order: /n";
+		String str ="order name: " + name +"  customer's ID: " + ID + "  number of books: " + numOfItems ;
+		str = str + "\n items in the order: \n";
 		for (int i = 0; i< numOfItems; i++) {
 			str = str + orderItems[i].toString();
 			System.out.println();
@@ -95,13 +147,13 @@ public class Order {
 		}
 		
 		double totalPrice = 0.0;
-		str = str + "/n total price: ";
+		
 		for (int i = 0; i< numOfItems; i++) {
 			totalPrice = totalPrice + orderItems[i].getPrice();
 			
 		}
 		
-		return str + totalPrice; 
+		return str + System.out.printf("Total Price: %.2f\n", totalPrice); 
 		
 	}
 	
